57f79ec36c295af408e98cbc8a58685eb04c2760
Jennifer Prince <jennifergraceprince@gmail.com>
2020-06-15 04:52:25 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,14 +1,14 @@
 module FancyDance
   module InstanceMethods
-    
+ 
     def twirl
-      "I'm twirling!
+      "I'm twirling!"
     end
-    
+ 
     def jump
       "Look how high I'm jumping!"
-    end 
-    
+    end
+ 
     def pirouette
       "I'm doing a pirouette"
     end
@@ -17,8 +17,9 @@ module FancyDance
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
   end
-  
+ 
   module ClassMethods
+ 
     def metadata
       "This class produces objects that love to dance."
     end
